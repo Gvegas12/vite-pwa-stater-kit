5fff9c07e4c0c5296443a58e748b2626f80323c6
abdukarimov.h2 <abdukarimov.h2@wildberries.work>
2023-09-20 17:00:56 +0300

    feat: add structure

---
+++ src/processes/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+// import styles from "./MainLayout.module.scss";
+
+interface IMainLayoutProps {}
+
+const MainLayout: FC<IMainLayoutProps> = () => {
+  return <div>MainLayout</div>;
+};
+
+export default MainLayout;
